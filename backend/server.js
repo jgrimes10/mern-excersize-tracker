25ce85c6cba05aec5e2bd0b52f785cf173e3ecba
Justin Grimes <jgrimes@relias.com>
2019-06-07 12:11:32 -0400

    set up server connection

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,29 @@
+// import dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+// set up middleware
 app.use(cors());
 app.use(bodyParser.json());
 
+// connect to db
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true})
+    .catch((err) => {
+        console.log(`Connection to MongoDB failed. Error: ${err}`)
+    });
+mongoose.connection.once('open', () => {
+    console.log('MongoDB database connection established successfully');
+});
+
+// start up server
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
